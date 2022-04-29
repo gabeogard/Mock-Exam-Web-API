2718626819456e2c6a1cde9ff5c757b8211aa87a
gabeogard <gaog001@student.kristiania.no>
2022-04-29 12:22:17 +0200

    there

--- server/server.js
+++ server/server.js
@@ -5,5 +5,14 @@ const app = express()
 
 app.use(express.static(path.resolve("../dist")))
 
+app.use((req, res, next) => {
+    if (req.method === "GET") {
+        // TODO: We probably should return 404 instead of index.html for api-calls as well
+        res.sendFile(path.resolve("client", "..", "..", "dist", "index.html"));
+    } else {
+        next();
+    }
+})
+
 app.listen(process.env.PORT || 3000)
 
