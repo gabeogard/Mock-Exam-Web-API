ecfed019d331891a892a2338be24921883bdfac4
gabeogard <gaog001@student.kristiania.no>
2022-04-29 12:19:08 +0200

    there

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import * as path from "path";
 
 const app = express()
 
-app.use(express.static(path.resolve("client","..","..","dist")))
+app.use(express.static(path.resolve("../dist")))
 
 app.listen(process.env.PORT || 3000)
 
