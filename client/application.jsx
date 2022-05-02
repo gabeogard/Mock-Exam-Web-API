c5d79bacc9bbb6635c27c21783c76e33da13b9f7
gabeogard <gaog001@student.kristiania.no>
2022-05-02 08:42:45 +0200

    fixed issue

--- client/application.jsx
+++ client/application.jsx
@@ -19,13 +19,28 @@ export async function postJSON(url, object) {
     }
 }
 
-
-export async function fetchJSON(url){
-    const res = await fetch(url)
-    if(!res.ok){
-        throw new Error(`Failed to load ${res.status}: ${res.statusText}`)
+export class HttpError extends Error {
+    constructor(status, statusText) {
+        super(statusText);
+        this.status = status;
     }
-    return await res.json()
+}
+
+
+
+export async function fetchJSON(url, requestInit) {
+    let res;
+    if (arguments.length === 1) {
+        res = await fetch(url);
+    } else res = await fetch(url, requestInit);
+    if (res.status === 204) {
+        console.log(204);
+        return null;
+    } else if (res.ok) {
+        return await res.json();
+    } else {
+        throw new HttpError(res.status, res.statusText);
+    }
 }
 
 export function Application() {
@@ -43,7 +58,7 @@ export function Application() {
         <Routes>
             <Route path={"/"} element={<FrontPage/>}/>
             <Route path={"/login/*"} element={<LogInPage config={data.config} reload={reload}/>}/>
-            <Route path={"/profile"} element={<Profile/>}/>
+            <Route path={"/profile"} element={<Profile user={data?.user}/>}/>
         </Routes>
     </BrowserRouter>
 }
--- client/profile.jsx
+++ client/profile.jsx
@@ -1,5 +1,27 @@
-export function Profile() {
+import {useLoading} from "./useLoading";
+import {fetchJSON} from "./application";
+
+export function Profile({user, signOut}) {
+
+    const {loading, data, error, userinfo} = useLoading(async () => {
+        return await fetchJSON("/api/login")
+    })
+
+    if(loading){
+        return <div>Please wait...</div>
+    }
+    if(error){
+        return <div>Error: {error.toString()}</div>
+    }
+
+    const {name, email} = data
+    console.log(name)
+    console.log(email)
+
+
     return <div>
-        <h1>Profile</h1>
+        <h1>Welcome, </h1>
+        <pre>add my google email and send me nudes </pre>
+        <button onClick={signOut}>Sign out</button>
     </div>
 }
